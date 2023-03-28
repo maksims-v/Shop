954b44b7e9ceb9568e4a9c9790c9844ab7b09b9f
maksims-v <spread2010@inbox.ru>
2023-03-28 17:48:23 +0300

    strapi add

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@mui/material';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from '@/state/authSlice.js';
+import { SessionProvider } from 'next-auth/react';
 
 const store = configureStore({
   reducer: {
@@ -13,16 +14,20 @@ const store = configureStore({
   },
 });
 
-const App = ({ Component, pageProps }) => (
-  <ThemeProvider theme={theme}>
-    <Provider store={store}>
-      <CssBaseline />
-      <Layout>
-        {' '}
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
-  </ThemeProvider>
-);
+const App = ({ Component, pageProps: { session, ...pageProps } }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <CssBaseline />
+        <Layout>
+          {' '}
+          <SessionProvider session={session}>
+            <Component {...pageProps} />
+          </SessionProvider>
+        </Layout>
+      </Provider>
+    </ThemeProvider>
+  );
+};
 
 export default App;
---
+++ client/src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,48 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch('/your/endpoint', {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          headers: { 'Content-Type': 'application/json' },
+        });
+        const user = await res.json();
+
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user;
+        }
+        // Return null if user data could not be retrieved
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.user = token;
+
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
