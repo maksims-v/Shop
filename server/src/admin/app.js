cef52b5227722d83dc75aa22bd00d5ed5da9d78a
maksims-v <spread2010@inbox.ru>
2023-08-16 18:19:57 +0300

    admin panel

---
+++ server/src/admin/app.js
@@ -0,0 +1,27 @@
+import logo from "./extensions/logo.png";
+import favicon from "./extensions/favicon.ico";
+
+const config = {
+  auth: {
+    logo: logo,
+  },
+  menu: {
+    logo: logo,
+  },
+  head: {
+    favicon: favicon,
+  },
+  locales: ["lv", "ru"],
+  tutorials: false,
+  // Disable notifications about new Strapi releases
+  notifications: { releases: false },
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
